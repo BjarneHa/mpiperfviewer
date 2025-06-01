9d709d8909c8e56306b1ba0d8228b8f1310fcde5
Bjarne Hansen <bjarne.hansen@tum.de>
2025-06-02 00:03:14 +0200

    Add decimal commas and si units to statistics

--- statistics_view.py
+++ statistics_view.py
@@ -1,7 +1,24 @@
+from math import inf, isnan
+
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QGridLayout, QGroupBox, QLabel, QWidget
 
 from parser import Rank
+
+SI_PREFIXES = "kMGTPEZYRQ"
+
+
+def si_str(n: float):
+    if n < 1000:
+        return f"{n:.3}"
+    if isnan(n) or n == inf or n == -inf:
+        return str(n)
+    m = n
+    for pref in SI_PREFIXES:
+        m /= 1000
+        if m < 1000:
+            return f"{m:.3} {pref}"
+    return f"{n:.3}"  # return exponential form
 
 
 class StatisticsView(QGroupBox):
@@ -13,18 +30,18 @@ class StatisticsView(QGroupBox):
         layout.addWidget(nodes_label, 0, 1)
         layout.addWidget(QLabel("#MPI Processes"), 1, 0)
         procs_label = QLabel(
-            text=str(rank_file.general().num_procs),
+            text=f"{rank_file.general().num_procs:,}",
             alignment=Qt.AlignmentFlag.AlignRight,
         )
         layout.addWidget(procs_label, 1, 1)
         layout.addWidget(QLabel("Total msg count"), 2, 0)
         total_msg_counts_label = QLabel(
-            text=str(rank_file.total_msgs_sent), alignment=Qt.AlignmentFlag.AlignRight
+            text=f"{rank_file.total_msgs_sent:,}", alignment=Qt.AlignmentFlag.AlignRight
         )
         layout.addWidget(total_msg_counts_label, 2, 1)
         layout.addWidget(QLabel("Total msg size"), 3, 0)
         total_msg_sizes_label = QLabel(
-            text=str(rank_file.total_bytes_sent) + " B",
+            text=si_str(rank_file.total_bytes_sent) + "B",
             alignment=Qt.AlignmentFlag.AlignRight,
         )
         layout.addWidget(total_msg_sizes_label, 3, 1)
